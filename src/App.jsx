c0d1a69fbffbb1337c997a25b7ad41d029e2dba7
ELHart05 <o.allaoua@esi-sba.dz>
2023-12-11 21:57:50 +0100

    added amina work

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import Navbar from "./components/Navbar"
 import Challeges from "./components/Challenges"
 import Activities from "./components/Activities"
 import Footer from "./components/Footer"
+import About from "./components/About"
 
 //TODO: after coding your template call it here to test it
 const App = () => {
   return (
     <main className="font-main">
       <Navbar />
+      <h1 className="py-24 text-center">hello</h1>
+      <About />
       <Challeges />
       <Activities />
       <Footer />
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -3,7 +3,12 @@ import './style.css'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='activities' className="bg-[url('/lineleft.svg')] bg-contain 2lg:pb-0 bg-no-repeat bg-[position:left] flex flex-col items-center justify-between gap-4 px-4 py-16 sm:px-20 sm:py-20 2md:px-4">
+      <h2 className='text-main font-black text-3xl sm:text-5xl'>Activities</h2>
+      <div className='font-bold text-xl max-w-4xl overflow-hidden flex items-center justify-start text-center w-full 2lg:pb-20 scrollbar-hide'>
+        Code2Exit is a day full of challenges in the field of IT where we will touch on several aspects such as: Front-end dev, Back-End dev, UI/UX and even design with activities that separate these challenges to change serious mode during challenges.
+      </div>
+  </section>
   )
 }
 
